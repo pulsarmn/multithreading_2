2ccb38f03a6b93f24471f123f557ed93431741fb
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-16 09:47:43 +0300

    add: implement the main() method in the Main class

--- src/main/java/com/pulsar/Main.java
+++ src/main/java/com/pulsar/Main.java
@@ -3,11 +3,24 @@ package com.pulsar;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Executors;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+        DataProcessor processor = new DataProcessor(Executors.newFixedThreadPool(10));
+
+        for (int i = 0; i < 100; i++) {
+            processor.process(getRandomList());
+        }
+
+        while (processor.getActiveCount() != 0) {}
+
+        System.out.println("Completed: " + processor.getCompetedCount());
+        for (int i = 1; i < 101; i++) {
+            System.out.println(processor.getResult("task-" + i));
+        }
+        processor.shutdown();
     }
 
     private static List<Integer> getRandomList() {

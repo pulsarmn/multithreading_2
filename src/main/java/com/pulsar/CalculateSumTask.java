e92c026e9e427d6d23741778b4b0284814aba400
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-16 09:36:16 +0300

    add: implement the call() method in the CalculateSumTask class

--- src/main/java/com/pulsar/CalculateSumTask.java
+++ src/main/java/com/pulsar/CalculateSumTask.java
@@ -14,7 +14,19 @@ public class CalculateSumTask implements Callable<Integer> {
     }
 
     @Override
-    public Integer call() throws Exception {
-        return 0;
+    public Integer call() {
+        System.out.printf("Задача %s начала своё выполнение в потоке %s%n", taskName, Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            System.out.printf("Задача %s была прервана в потоке %s%n", taskName, Thread.currentThread().getName());
+        }
+
+        if (numbers == null) return 0;
+
+        return numbers.stream()
+                .mapToInt(value -> value)
+                .sum();
     }
 }

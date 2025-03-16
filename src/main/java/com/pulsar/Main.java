e4fa29a892fbd6504adb054b55762b1e33cef287
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-16 09:41:21 +0300

    add: implement the getRandomList() method in the Main class

--- src/main/java/com/pulsar/Main.java
+++ src/main/java/com/pulsar/Main.java
@@ -1,8 +1,23 @@
 package com.pulsar;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        
+    }
 
+    private static List<Integer> getRandomList() {
+        Random random = new Random();
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < random.nextInt(0, 100); i++) {
+            result.add(random.nextInt(1, 500));
+        }
+
+        return result;
     }
 }

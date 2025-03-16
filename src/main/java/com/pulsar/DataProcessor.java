06dd0a43b4b39150278b2a75df7913c79b46eb46
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-16 09:37:13 +0300

    add: create the DataProcessor class

---
+++ src/main/java/com/pulsar/DataProcessor.java
@@ -0,0 +1,26 @@
+package com.pulsar;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class DataProcessor {
+
+    private final AtomicInteger completedTasksCounter = new AtomicInteger(0);
+    private final AtomicInteger activeTasks = new AtomicInteger(0);
+
+    private final ExecutorService executorService;
+    private final Map<String, Integer> taskResult;
+    private final Object lock = new Object();
+
+    public DataProcessor() {
+        this(Executors.newCachedThreadPool());
+    }
+
+    public DataProcessor(ExecutorService executorService) {
+        this.executorService = executorService;
+        this.taskResult = new HashMap<>();
+    }
+}
